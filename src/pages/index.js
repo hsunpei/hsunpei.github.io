441487469937cdd154043d5c59bb2d4f1ea4c6a3
garfieldduck <garfieldduck@gmail.com>
2017-12-04 18:14:41 +0800

    Add card layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,9 @@ import Markdown from 'react-remarkable'
 import React from 'react'
 import styled from 'styled-components'
 import SelfIntro from '../text/self-intro'
-import { colors } from '../styles/variables'
+import { colors, animations } from '../styles/variables'
 import mq from '../styles/media-queries'
 import GlobalWrapper from '../components/GlobalWrapper'
-
-const Title = styled.h1`
-  color: ${colors.darkText};
-  font-size: 50px;
-`
 
 const Container = styled.div`
   max-width: 62rem;
@@ -22,18 +17,59 @@ const Container = styled.div`
   `}
 `
 
+const Title = styled.h1`
+  color: ${colors.darkText};
+  font-size: 50px;
+`
+
+const IntroBox = styled.div`
+  max-width: 40rem;
+`
+
+const WorkWrapper = styled.div`
+  width: 100%;
+`
+
+const Card = styled.div`
+  cursor: pointer;
+  background-color: ${colors.white};
+  box-shadow: 0 2px 3px 0 rgba(0, 0, 0, .15);
+  border-radius: 3px;
+  padding: 1rem;
+  max-width: 100%;
+  width: 12.5rem;
+  margin: 0;
+  ${animations.moveUp}
+
+  ${mq.lessThan('mobile')`
+    width: 100%;
+    padding: 0;
+  `}
+
+  p {
+    margin: 0.5rem;
+  }
+`
+
 class Home extends React.Component {
   render() {
     GlobalWrapper()
     return (
       <Container>
-        <Title>My page</Title>
-        {/* Pass Markdown source to the `source` prop */}
-        <Markdown source="**Markdown is awesome!**" />
+        <IntroBox>
+          <Title>My page</Title>
+          {/* Pass Markdown source to the `source` prop */}
+          <Markdown source="**Markdown is awesome!**" />
 
-        {/* Or pass it as children */}
-        {/* You can nest React components, too */}
-        <Markdown source={SelfIntro} options={{ linkTarget: '_blank' }} />
+          {/* Or pass it as children */}
+          {/* You can nest React components, too */}
+          <Markdown source={SelfIntro} options={{ linkTarget: '_blank' }} />
+        </IntroBox>
+        <WorkWrapper>
+          <Card>
+            <p>No-Kill Policy - The New Paradise for Homeless Animals?</p>
+          </Card>
+        </WorkWrapper>
       </Container>
     )
   }
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,7 +1,18 @@
-const colors = {
+export const colors = {
   lightGray: '#fcfefe',
   mainColor: '#28cc90',
   darkText: '#101935',
+  white: '#ffffff',
 }
 
-export { colors }
+export const animations = {
+  moveUp: `
+    position: relative;
+    top: 0;
+    transition: top 0.3s ease;
+
+    &:hover {
+      top: -0.2rem;
+    }
+  `,
+}
