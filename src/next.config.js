d070b15107f5c759f55c0cbef95673b22c6f2345
garfieldduck <garfieldduck@gmail.com>
2017-12-05 19:19:07 +0800

    Add image compression

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,39 @@
 module.exports = {
+  webpack: (config, { dev }) => {
+    config.module.rules.push({
+      test: /\.(gif|png|jpe?g)$/i,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 65,
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4,
+            },
+            gifsicle: {
+              interlaced: false,
+              optimizationLevel: 2,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75,
+            },
+          },
+        },
+      ],
+    })
+    return config
+  },
+
   exportPathMap() {
     return {
       '/': { page: '/' },
