9f4afed22017e969b978bc4cfd826ef80a5a92e8
garfieldduck <garfieldduck@gmail.com>
2017-12-07 19:13:42 +0800

    Update og image meta

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   appConfig: {
     projectName: 'garfieldduck.github.io',
-    url: 'https://garfieldduck.github.io/',
+    url: 'https://garfieldduck.github.io',
     assetPrefix: '/',
     siteName: 'HsunPei Wang',
     description: 'Passionate full-stack developer specialized in Javascript. Works on projects related to data visualization and visual journalism. Hobby graphics designer. Lives in Taipei.',
