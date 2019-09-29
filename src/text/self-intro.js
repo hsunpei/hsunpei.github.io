9b1d7e2fbb6cc26cb560204c65d58833c6503baf
garfieldduck <garfieldduck@gmail.com>
2019-09-29 19:36:56 +0200

    Update the self-intro

--- src/text/self-intro.js
+++ src/text/self-intro.js
@@ -1,5 +1,7 @@
 const SelfIntro = `
-Passionate full-stack developer specialized in Javascript. Works on projects related to data visualization and visual journalism. 4 years of web development experience related to ReactJS/Redux, d3, and CSS/SASS/StyledComponents; basic knowledge in GoLang and Docker.
+Passionate front-end engineer with 6+ years of front-end UI and data visualization development experience.
+
+Interested in: ReactJS, d3, GraphQL, and Typescript.
 
 Creative web developer dedicates to cooperating with people in NGOs to explore new ways of technology integrations to foster the communication and discussion of social issues.
 
